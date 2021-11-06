f2328f8f7d8c880c1b0a1f9569afb707752e3052
kokoryh <kokoryh@qq.com>
2021-11-06 22:01:09 +0800

    Update IP2.js

--- IP2.js
+++ IP2.js
@@ -9,7 +9,7 @@ var obj = JSON.parse(body);
 var title = flags.get(obj['countryCode']) + ' ' + obj['tag'];
 var subtitle = obj['isp'];
 var ip = obj['query'];
-var description = obj;
+var description = toString(obj);
 
 
 $done({title, subtitle, ip, description});
